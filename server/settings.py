8a714136772e507e909445ac27d1199993da614a
Hans Queja <hansqueja8@gmail.com>
2026-01-28 12:31:01 +0800

    fix: origins trailing slash removed

--- server/settings.py
+++ server/settings.py
@@ -156,7 +156,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://gradely-frontend-lovat.vercel.app/", 
+    "https://gradely-frontend-lovat.vercel.app", 
 ]
 
 CORS_ALLOW_CREDENTIALS = True
